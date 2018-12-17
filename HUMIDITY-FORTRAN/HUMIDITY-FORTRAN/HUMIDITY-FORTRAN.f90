f79de81bc510962cd7fcb470cac1af7c7580f739
grgsiberia <nanashi4129@gmail.com>
2018-12-17 19:06:12 +0900

    とりあえず，ソリューションをアップ

---
+++ HUMIDITY-FORTRAN/HUMIDITY-FORTRAN/HUMIDITY-FORTRAN.f90
@@ -0,0 +1,29 @@
+!  HUMIDITY_FORTRAN.f90 
+!
+!  関数:
+!  HUMIDITY_FORTRAN - コンソール・アプリケーションのエントリーポイント。
+!
+
+!****************************************************************************
+!
+!  プログラム: HUMIDITY_FORTRAN
+!
+!  目的:  コンソール・アプリケーションのエントリーポイント。
+!
+!****************************************************************************
+
+    program HUMIDITY_FORTRAN
+
+    implicit none
+
+    ! 変数
+    real width, height, depth   ! 部屋の大きさ
+    real temperature, humidity  ! 気温，湿度
+    real atmospheric_pressure   ! 気圧
+    real water_liter            ! 水の容量
+    
+    ! HUMIDITY_FORTRAN の本文
+    print *, 'Hello World'
+
+    end program HUMIDITY_FORTRAN
+
